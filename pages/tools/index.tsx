7ab44af3b7cd89484d47a236d5a1b6399603e79c
Zifan Hua <zifan.hua@icloud.com>
2023-11-04 18:32:51 +0000

    Add base64 decoder
    Signed-off-by: Zifan Hua <zifan.hua@icloud.com>

--- pages/tools/index.tsx
+++ pages/tools/index.tsx
@@ -1,6 +1,8 @@
 import Layout from "@/components/layout";
 import Config from "@/interfaces/config";
 import getConfig from "@/lib/config";
+import Head from "next/head";
+import Link from "next/link";
 
 interface IntroPageProps {
   config: Config;
@@ -9,19 +11,33 @@ interface IntroPageProps {
 const IntroPage: React.FC<IntroPageProps> = ({ config }) => {
   return (
     <Layout config={config}>
-      This is some tools on the site.
+      Here is some tools on the site.
       <br />
       All the tools are run in the browser, so no data is sent to the server.
       <br />
       Encode and Decode:
-      <ul
-        className="ml-5"
-      >
+      <ul className="ml-5">
         <li>
-          <a href="/tools/base64/encode"
-            className="underline"
-          >Base64 Encode</a>
+          <Link href="/tools/base64/encode" className="underline">
+            Base64 Encode
+          </Link>
         </li>
+        <li>
+          <Link href="/tools/base64/decode" className="underline">
+            Base64 Decode
+          </Link>
+        </li>
+        {
+          // TODO qrcode generator
+          // TODO qrcode reader
+          // TODO add inch to cm
+          // TODO add cm to inch
+          // TODO add fahrenheit to celsius
+          // TODO add celsius to fahrenheit
+          // TODO url encode
+          // TODO url decode
+          // TODO regex tester
+        }
       </ul>
     </Layout>
   );
