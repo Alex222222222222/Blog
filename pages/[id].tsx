72e36bf81cda0899991fb28e78f2b716633d375f
Zifan Hua <zifan.hua@icloud.com>
2023-11-08 23:46:43 +0000

    fix some bugs
    Signed-off-by: Zifan Hua <zifan.hua@icloud.com>

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -2,7 +2,7 @@
 import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
 import { ParsedUrlQuery } from "querystring";
 import { get_posts_paths } from "@/lib/markdown_file_meta";
-import { permanentRedirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 interface PostProps {
@@ -10,8 +10,9 @@ interface PostProps {
 }
 
 const PostPage: React.FC<PostProps> = ({ id }) => {
+  const router = useRouter();
   useEffect(() => {
-    permanentRedirect(`/posts/${id}`);
+    router.push(`/posts/${id}`);
   });
 
   return <div>Redirecting...</div>;
@@ -20,12 +21,29 @@ const PostPage: React.FC<PostProps> = ({ id }) => {
 export default PostPage;
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = get_posts_paths().map((filename) => ({
-    params: { id: filename },
-  }));
+  let paths: { params: { id: string } }[] = [];
+
+  get_posts_paths().forEach((filename) => {
+    // remove the .md extension if it exists
+    const id = filename.replace(/\.md$/, "");
+    paths.push({
+      params: { id: id.toLowerCase() },
+    });
+    paths.push({
+      params: { id: filename.toLowerCase() },
+    });
+  });
+
+  const path_set = new Set(paths);
+
+  const paths_new = Array.from(path_set).map((path) => {
+    return {
+      params: path.params,
+    };
+  });
 
   return {
-    paths,
+    paths: paths_new,
     fallback: false,
   };
 };
@@ -34,6 +52,26 @@ export const getStaticProps: GetStaticProps = async (
   context: GetStaticPropsContext<ParsedUrlQuery>
 ) => {
   const { id } = context.params as { id: string };
+  const id_lowercase = id.toLowerCase();
+  let paths = get_posts_paths();
+  for (let i = 0; i < paths.length; i++) {
+    const filename = paths[i];
+    const f = filename.replace(/\.md$/, "");
+    const f_lowercase = f.toLowerCase();
+    if (id_lowercase === f_lowercase) {
+      return {
+        props: {
+          id,
+        },
+      };
+    } else if (filename.toLowerCase() === id_lowercase) {
+      return {
+        props: {
+          id: filename,
+        },
+      };
+    }
+  }
 
   return {
     props: {
