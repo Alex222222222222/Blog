c77245ecf59100e98e6e05076e8beff1548fd973
Zifan Hua <zifan.hua@icloud.com>
2023-11-16 23:46:30 +0000

    Change to github next monaspace krypton font
    Signed-off-by: Zifan Hua <zifan.hua@icloud.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,86 @@
 import "@/styles/globals.css";
 import { AppProps } from "next/app";
+import localFont from 'next/font/local'
+
+const krypton = localFont({
+  src: [
+    {
+      path: '../fonts/MonaspaceKrypton-Bold.woff',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/MonaspaceKrypton-BoldItalic.woff',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/MonaspaceKrypton-ExtraBold.woff',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/MonaspaceKrypton-ExtraBoldItalic.woff',
+      weight: '800',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/MonaspaceKrypton-ExtraLight.woff',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/MonaspaceKrypton-ExtraLightItalic.woff',
+      weight: '200',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/MonaspaceKrypton-Italic.woff',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/MonaspaceKrypton-Light.woff',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/MonaspaceKrypton-LightItalic.woff',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/MonaspaceKrypton-Medium.woff',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/MonaspaceKrypton-MediumItalic.woff',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/MonaspaceKrypton-Regular.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/MonaspaceKrypton-SemiBold.woff',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/MonaspaceKrypton-SemiBoldItalic.woff',
+      weight: '600',
+      style: 'italic',
+    },
+  ],
+})
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={krypton.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
