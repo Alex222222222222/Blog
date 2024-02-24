ee8b1d6e71ef21edaffc65886165be8ea0864144
Zifan Hua <zifan.hua@icloud.com>
2024-02-24 21:20:10 +0000

    Add new words
    Signed-off-by: Zifan Hua <zifan.hua@icloud.com>

--- components/footbar.tsx
+++ components/footbar.tsx
@@ -8,11 +8,17 @@ const FootBar: React.FC = () => {
       {
         // for each word in the words.json file, create a <p> tag
       }
-      {words[randomWord].map((word: string, index: number) => (
-        <p key={index} className="flex items-center justify-center">
-          {word}
-        </p>
-      ))}
+      {words[randomWord].map((word: string, index: number) => {
+        if (word.trim().length === 0) {
+          return <br key={index} />;
+        }
+
+        return (
+          <p key={index} className="flex items-center justify-center">
+            {word}
+          </p>
+        );
+      })}
     </div>
   );
 };
