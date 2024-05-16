64ec6232cc5862677cbc29212e0590be1693322b
Zifan Hua <zifan.hua@icloud.com>
2024-05-16 18:46:19 +0100

    Add total site view number
    Signed-off-by: Zifan Hua <zifan.hua@icloud.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import "@/styles/globals.css";
 import { AppProps } from "next/app";
 import localFont from "next/font/local";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-// const NEXT_PUBLIC_GA_ID = "G-49DZRF2KTW";
+const NEXT_PUBLIC_GA_ID = "G-49DZRF2KTW";
 
 const krypton = localFont({
   src: [
@@ -80,37 +82,37 @@ const krypton = localFont({
 });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  // let trackingJSUrl = `https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GA_ID}`;
+  let trackingJSUrl = `https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GA_ID}`;
 
-  // const router = useRouter();
-  // useEffect(() => {
-  //   const handleRouteChange = (url: any) => {
-  //     window.gtag("config", NEXT_PUBLIC_GA_ID, {
-  //       page_path: url,
-  //     });
-  //   };
-  //   router.events.on("routeChangeComplete", handleRouteChange);
-  //   return () => {
-  //     router.events.off("routeChangeComplete", handleRouteChange);
-  //   };
-  // }, [router.events]);
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      window.gtag("config", NEXT_PUBLIC_GA_ID, {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <main className={krypton.className}>
+      {<script async src={trackingJSUrl}></script>}
       {
-        // <script async src={trackingJSUrl}></script>
-        // <script
-        //   dangerouslySetInnerHTML={{
-        //     __html: `
-        //           window.dataLayer = window.dataLayer || [];
-        //           function gtag(){dataLayer.push(arguments);}
-        //           gtag('js', new Date());
-        //           gtag('config', '${NEXT_PUBLIC_GA_ID}', {
-        //             page_path: window.location.pathname
-        //           });
-        //         `,
-        //   }}
-        // />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${NEXT_PUBLIC_GA_ID}', {
+                    page_path: window.location.pathname
+                  });
+                `,
+          }}
+        />
       }
       <Component {...pageProps} />
     </main>
