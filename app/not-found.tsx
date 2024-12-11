0903706ec4fba21d5cae1506b624b430c08d80df
Zifan Hua <zifan.hua@icloud.com>
2024-12-11 00:10:54 +0000

    Add 404 page
    Signed-off-by: Zifan Hua <zifan.hua@icloud.com>

---
+++ app/not-found.tsx
@@ -0,0 +1,3 @@
+export default async function Page() {
+  return <h1>404 - Page Not Found</h1>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -5,40 +5,6 @@ import HomeCredits from "@/components/homeCredits";
 import SeparateLine from "@/components/hr";
 import PostList from "@/components/postList";
 
-/**
-interface HomeProps {
-  posts: Post[];
-}
-
-const Home: React.FC<HomeProps> = ({ posts }) => {
-  return (
-    <Layout>
-      <div>
-        <HomeAbout />
-      </div>
-      <SeparateLine />
-      <div>
-        {<PostList posts={posts} search="" categories={[]} tags={[]} />}
-      </div>
-    </Layout>
-  );
-};
-
-export default Home;
-
-export const getStaticProps = async () => {
-  await preBuild();
-
-  // TODO add search whole site
-
-  return {
-    props: {
-      posts: await get_empty_posts(),
-    },
-  };
-};
-*/
-
 export default async function Page() {
   return (
     <>
