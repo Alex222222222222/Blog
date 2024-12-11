558a6ec2cfcfdd54b3413414c6dc93a8c8231e62
Zifan Hua <zifan.hua@icloud.com>
2024-12-11 00:35:51 +0000

    Add categories page and category page
    Signed-off-by: Zifan Hua <zifan.hua@icloud.com>

---
+++ app/categories/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import fs from "fs";
+import Post from "@/interfaces/post";
+
+export const dynamic = "force-static";
+
+export default async function Page() {
+  const postsJson = fs.readFileSync(".build_cache/res/allPosts.json", "utf-8");
+  const posts: Post[] = JSON.parse(postsJson.toString());
+  const categories = new Set<string>();
+  posts.forEach((post) => {
+    post.categories.forEach((category) => {
+      categories.add(category);
+    });
+  });
+  const categoriesArray = Array.from(categories);
+
+  return (
+    <>
+      <h1>Categories:</h1>
+      <ul className="grid grid-cols-2 ml-5 underline">
+        {categoriesArray.map((category, index) => (
+          <li key={index}>
+            <a href={`/categories/${category}`}>{category}</a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
