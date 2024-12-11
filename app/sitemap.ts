de8cd5a9be8dc5c38682e3253927a6f8673c2363
Zifan Hua <zifan.hua@icloud.com>
2024-12-11 00:06:26 +0000

    Generate sitemap.xml for pages
    Signed-off-by: Zifan Hua <zifan.hua@icloud.com>

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,44 +1,105 @@
 import type { MetadataRoute } from "next";
-import generateSitemap from "@/lib/sitemap";
-import { get_all_posts } from "@/lib/markdown_file_meta";
 import getConfig from "@/lib/config";
+import Config from "@/interfaces/config";
+import fs from "fs";
+import Post from "@/interfaces/post";
 
 export const dynamic = "force-static";
 
-/**
- * // TODO
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const posts = await get_all_posts();
   const config = getConfig();
-  const sitemap = await generateSitemap(posts, config);
-
-  // TODO check whether it can be both accessed at /sitemap.xml and /sitemap
-  // fs.writeFileSync("public/sitemap.xml", sitemap);
-  // fs.writeFileSync("public/sitemap", sitemap);
+  const sitemap = await generateSitemap(config);
 
   return sitemap;
 }
-  */
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://acme.com',
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
-    },
-    {
-      url: 'https://acme.com/about',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://acme.com/blog',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
-  ]
-}
+const otherPages = [
+  {
+    url: "/",
+    lastmod: new Date().toISOString(),
+  },
+  {
+    url: "/tags",
+    lastmod: new Date().toISOString(),
+  },
+  {
+    url: "/categories",
+    lastmod: new Date().toISOString(),
+  },
+  {
+    url: "/tools",
+    lastmod: new Date().toISOString(),
+  },
+  {
+    url: "/tools/base64/encode",
+    lastmod: new Date().toISOString(),
+  },
+  {
+    url: "/tools/base64/decode",
+    lastmod: new Date().toISOString(),
+  },
+  {
+    url: "/tools/base64/encode",
+    lastmod: new Date().toISOString(),
+  },
+  {
+    url: "tools/qrcode/generator",
+    lastmod: new Date().toISOString(),
+  },
+  {
+    url: "tools/qrcode/reader",
+    lastmod: new Date().toISOString(),
+  },
+  {
+    url: "tools/stable_diffusion",
+    lastmod: new Date().toISOString(),
+  },
+  {
+    url: "tools/pdf2pic",
+    lastmod: new Date().toISOString(),
+  },
+  {
+    url: "tools/password_gen",
+    lastmod: new Date().toISOString(),
+  },
+];
+
+const generateSitemap = async (
+  config: Config
+): Promise<MetadataRoute.Sitemap> => {
+  const postsJson = fs.readFileSync(".build_cache/res/allPosts.json", "utf-8");
+  const posts: Post[] = JSON.parse(postsJson.toString());
+
+  let baseUrls = config.sitemapBaseUrl;
+  if (!baseUrls) {
+    baseUrls = [config.baseUrl];
+  }
+  if (!baseUrls.includes(config.baseUrl)) {
+    baseUrls.push(config.baseUrl);
+  }
+
+  const sitemap: MetadataRoute.Sitemap = [];
+  baseUrls.forEach((baseUrl) => {
+    posts.forEach((post) => {
+      const url = new URL(`/posts/${post.filename}`, baseUrl).toString();
+      sitemap.push({
+        url,
+        lastModified: new Date(post.date).toISOString(),
+        changeFrequency: "monthly",
+        priority: 0.8,
+      });
+    });
+
+    otherPages.forEach((page) => {
+      const url = new URL(page.url, baseUrl).toString();
+      sitemap.push({
+        url,
+        lastModified: page.lastmod,
+        changeFrequency: "monthly",
+        priority: 0.8,
+      });
+    });
+  });
+
+  return sitemap;
+};
