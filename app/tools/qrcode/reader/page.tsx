2870f37c978c73870f77460ae10fcbe7197b1b53
Zifan Hua <zifan.hua@icloud.com>
2024-12-11 14:43:17 +0000

    Add page.tsx to qrcode reader
    Signed-off-by: Zifan Hua <zifan.hua@icloud.com>

---
+++ app/tools/qrcode/reader/page.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import QRScanner from "qr-scanner";
+import Layout from "@/components/layout";
+import { Metadata } from "next";
+import QRCodeReader from "@/components/qrcode/reader";
+
+export const metadata: Metadata = {
+  title: "QR Code Reader",
+  description: "Read QR code from an image",
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>QR Code Reader</h1>
+      <QRCodeReader />
+    </>
+  );
+}
