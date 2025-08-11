255a83c0cc889a339c28d3aee8e9da2b63133a21
Zifan Hua <zifan.hua@icloud.com>
2025-08-11 20:06:42 +0800

    Add Typst SVG rendering for math and inline math
    Render math and inline math blocks as SVG using Typst, replacing KaTeX.
    Add CSS for inline math SVG display. Refactor AST traversal for math
    rendering.

--- lib/markdown_file_meta.ts
+++ lib/markdown_file_meta.ts
@@ -83,18 +83,18 @@ export function get_date_from_filename(filename: string): Date {
 }
 
 export async function get_markdown_file(
-  filename: string,
+  filename: string
 ): Promise<[string | null, string]> {
   const isDir = fs.lstatSync(path.join("posts", filename)).isDirectory();
   if (isDir) {
     const dir = fs.readdirSync(path.join("posts", filename));
     const mdFile = dir.find(
-      (file) => file === "index.md" || file === `${filename}.md`,
+      (file) => file === "index.md" || file === `${filename}.md`
     );
     if (mdFile) {
       // get the last modified date
       const last_modified = await getLastModifiedDate(
-        path.join("posts", filename, mdFile),
+        path.join("posts", filename, mdFile)
       );
 
       return [
@@ -131,7 +131,7 @@ function add_0_to_date(date: number): string {
 /// filter out nulls
 /// map the remaining files to the Post interface
 export async function get_markdown_data(
-  filename: string,
+  filename: string
 ): Promise<Post | null> {
   // get from cache
   const key = getHashKey(filename);
@@ -195,9 +195,9 @@ export async function get_all_posts(): Promise<Post[]> {
 
   const files = fs.readdirSync(path.join("posts"));
   const posts: (Post | null)[] = await Promise.all(
-    files.map(async (filename) => {
-      return await get_markdown_data(filename);
-    }),
+    files.map((filename) => {
+      return get_markdown_data(filename);
+    })
   );
   const valid_posts = posts.filter((post) => post !== null) as Post[];
   // sort by date descending
@@ -291,7 +291,7 @@ export async function get_posts_paths_with_alias(): Promise<string[]> {
 
 /// export find matching paths with alias
 export async function find_matching_paths_with_alias(
-  path: string,
+  path: string
 ): Promise<string | undefined> {
   // check the cache first
   const key = getHashKey(`matching_paths_with_alias_${path}`);
@@ -324,7 +324,7 @@ export async function find_matching_paths_with_alias(
 
 /// export get previous and next posts
 export async function get_previous_and_next_posts(
-  path: string,
+  path: string
 ): Promise<[string | null, string | null]> {
   // check the cache first
   const key = getHashKey(`previous_and_next_posts_${path}`);
@@ -399,7 +399,7 @@ export async function get_all_categories(): Promise<string[]> {
 /// parse markdown to html
 async function parseMarkdown2Html(
   markdown: string,
-  toc: Boolean,
+  toc: Boolean
 ): Promise<string> {
   const key = getHashKey(markdown);
   const cachedFile = readCache("markdown", key);
@@ -415,7 +415,6 @@ async function parseMarkdown2Html(
     .use(remarkGfm)
     .use(remarkAutoNumberHeadings)
     .use(remarkMath)
-    .use(remarkToc, { heading: "0.1 Contents" })
     .use(remarkMathEnv, {
       theoremEnvs: new Map([
         ["theorem", defaultTheoremOptions("theorem")],
@@ -428,8 +427,9 @@ async function parseMarkdown2Html(
       ]),
     })
     .use(remarkTypstSupport)
+    .use(remarkToc, { heading: "0.1 Contents" })
     .use(remarkRehype, remarkRehypeOptions)
-    .use(rehypeKatex)
+    //  .use(rehypeKatex)
     .use(rehypeRaw)
     .use(rehypeSlug)
     .use(rehypeHeadingLink)
--- lib/remarkTypstSupport.ts
+++ lib/remarkTypstSupport.ts
@@ -1,7 +1,9 @@
 import { NodeCompiler } from "@myriaddreamin/typst-ts-node-compiler";
-import { Root, RootContent } from "mdast";
+import { Html, PhrasingContent, Root, RootContent } from "mdast";
 import crypto from "crypto";
 import fs from "fs";
+
+const mathFontSize = 20;
 
 /**
  * Add `auto-numbering` to headings in Markdown.
@@ -13,41 +15,258 @@ import fs from "fs";
  */
 export function remarkTypstSupport() {
   return async (tree: Root): Promise<undefined> => {
-    const newChildren: RootContent[] = [];
+    const newChildren: RootContent[] = await Promise.all(
+      tree.children.map((node) => renderNode(node))
+    );
 
-    // Visit each code block node in the Markdown AST
-    for (var node of tree.children) {
-      if (node.type === "code" && node.lang === "typst") {
-        // compile the typst to svg
-        const svg = NodeCompiler.create().svg({
-          mainFileContent: `
-            #set page(width: auto, height: auto, margin: 10pt)
-            #import "@preview/commute:0.3.0": *
+    tree.children = newChildren;
+  };
+}
 
-            ${node.value}
-          `,
-        });
-        const key = crypto.createHash("sha1").update(node.value).digest("hex");
-
-        // check if dir ./public/typst_svg/ exists
-        if (!fs.existsSync("./public/typst_svg/")) {
-          fs.mkdirSync("./public/typst_svg/", { recursive: true });
-        }
-        fs.writeFileSync(`./public/typst_svg/${key}.svg`, svg);
-        const svg_path = `/typst_svg/${key}.svg`;
-
-        newChildren.push({
+async function renderNode(node: RootContent): Promise<any> {
+  if (node.type === "code" && node.lang === "typst") {
+    // render typst code block
+    // compile the typst to svg
+    try {
+      // check if dir ./public/typst_svg/ exists
+      if (!fs.existsSync("./public/typst_svg/")) {
+        fs.mkdirSync("./public/typst_svg/", { recursive: true });
+      }
+      const key = crypto.createHash("sha1").update(node.value).digest("hex");
+      const svg_path = `/typst_svg/${key}.svg`;
+      // check if `./public/typst_svg/${key}.svg` already exists
+      if (fs.existsSync(`./public/typst_svg/${key}.svg`)) {
+        return {
           type: "html",
           value: `
           <object type="image/svg+xml" data="${svg_path}">
-            <img src="${svg_path}" alt="typst-image"/>
+            <img src="${svg_path}" alt="${node.value}"/>
           </object>`,
-        });
-      } else {
-        newChildren.push(node);
+        };
       }
 
-      tree.children = newChildren;
+      const svg = NodeCompiler.create().svg({
+        mainFileContent: `
+            #set page(width: auto, height: auto, margin: 10pt)
+            #import "@preview/commute:0.3.0": *
+            #set text( size: ${mathFontSize}pt )
+
+            ${node.value}
+          `,
+      });
+
+      fs.writeFileSync(`./public/typst_svg/${key}.svg`, svg);
+
+      return {
+        type: "html",
+        value: `
+          <object type="image/svg+xml" data="${svg_path}">
+            <img src="${svg_path}" alt="${node.value}"/>
+          </object>`,
+      };
+    } catch (e) {
+      console.log(
+        "Error rendering math, error:\n",
+        e,
+        `\nmath:\n${node.value}`
+      );
+
+      return node;
     }
-  };
+  } else if (node.type === "math") {
+    // render math block
+    // compile the typst to svg
+    return renderMath(node);
+  } else if (node.type === "inlineMath") {
+    // render inline math block
+    // compile the typst to svg
+    return renderMath(node);
+  } else if (node.type === "paragraph") {
+    node.children = await Promise.all(
+      node.children.map(async (node) => renderNode(node))
+    );
+    return node;
+  } else if (node.type === "blockquote") {
+    node.children = await Promise.all(
+      node.children.map((node) => renderNode(node))
+    );
+    return node;
+  } else if (node.type === "emphasis") {
+    node.children = await Promise.all(
+      node.children.map((node) => renderNode(node))
+    );
+    return node;
+  } else if (node.type === "footnoteDefinition") {
+    node.children = await Promise.all(
+      node.children.map((node) => renderNode(node))
+    );
+    return node;
+  } else if (node.type === "heading") {
+    node.children = await Promise.all(
+      node.children.map((node) => renderNode(node))
+    );
+    return node;
+  } else if (node.type === "list") {
+    node.children = await Promise.all(
+      node.children.map(async (node) => {
+        node.children = await Promise.all(
+          node.children.map((node) => renderNode(node))
+        );
+        return node;
+      })
+    );
+    return node;
+  } else if (node.type === "listItem") {
+    node.children = await Promise.all(
+      node.children.map((node) => renderNode(node))
+    );
+    return node;
+  } else if (node.type === "strong") {
+    node.children = await Promise.all(
+      node.children.map((node) => renderNode(node))
+    );
+    return node;
+  } else if (node.type === "table") {
+    node.children = await Promise.all(
+      node.children.map(async (node) => {
+        node.children = await Promise.all(
+          node.children.map(async (node) => {
+            node.children = await Promise.all(
+              node.children.map((node) => renderNode(node))
+            );
+            return node;
+          })
+        );
+        return node;
+      })
+    );
+    return node;
+  } else if (node.type === "tableCell") {
+    node.children = await Promise.all(
+      node.children.map((node) => renderNode(node))
+    );
+    return node;
+  } else if (node.type === "tableRow") {
+    node.children = await Promise.all(
+      node.children.map(async (node) => {
+        node.children = await Promise.all(
+          node.children.map((node) => renderNode(node))
+        );
+        return node;
+      })
+    );
+    return node;
+  } else {
+    return node;
+  }
+}
+
+async function renderMath(node: any): Promise<any> {
+  if (node.type == "math") {
+    let res = await renderMathInner(node.value);
+    if (res) {
+      return res;
+    } else {
+      return node;
+    }
+  } else if (node.type == "inlineMath") {
+    let res = await renderInlineMathInner(node.value);
+    if (res) {
+      return res;
+    } else {
+      return node;
+    }
+  } else {
+    return node;
+  }
+}
+
+async function renderInlineMathInner(value: string): Promise<Html | null> {
+  // render inline math block
+  // compile the typst to svg
+  try {
+    // check if dir ./public/typst_svg/ exists
+    if (!fs.existsSync("./public/typst_svg/")) {
+      fs.mkdirSync("./public/typst_svg/", { recursive: true });
+    }
+    const key = crypto.createHash("sha1").update(value).digest("hex");
+    const svg_path = `/typst_svg/${key}.svg`;
+    // check if `./public/typst_svg/${key}.svg` already exists
+    if (fs.existsSync(`./public/typst_svg/${key}.svg`)) {
+      return {
+        type: "html",
+        value: `<img src="${svg_path}" class="inline_math_svg" alt="${value}"/>`,
+      };
+    }
+
+    const svg = NodeCompiler.create().svg({
+      mainFileContent: `
+            #set page(width: auto, height: auto, margin: 1pt)
+            #import "@preview/commute:0.3.0": *
+            #import "@preview/mitex:0.2.5": *
+            #set text( size: ${mathFontSize}pt )
+
+            #mitex(\` ${value} \`)
+          `,
+    });
+
+    fs.writeFileSync(`./public/typst_svg/${key}.svg`, svg);
+
+    return {
+      type: "html",
+      value: `<img src="${svg_path}" class="inline_math_svg" alt="${value}"/>`,
+    };
+  } catch (e) {
+    console.log("Error rendering math, error:\n", e, `\nmath:\n${value}`);
+
+    return null;
+  }
+}
+
+async function renderMathInner(value: string): Promise<Html | null> {
+  // render math block
+  // compile the typst to svg
+  try {
+    // check if dir ./public/typst_svg/ exists
+    if (!fs.existsSync("./public/typst_svg/")) {
+      fs.mkdirSync("./public/typst_svg/", { recursive: true });
+    }
+    const key = crypto.createHash("sha1").update(value).digest("hex");
+    const svg_path = `/typst_svg/${key}.svg`;
+    // check if `./public/typst_svg/${key}.svg` already exists
+    if (fs.existsSync(`./public/typst_svg/${key}.svg`)) {
+      return {
+        type: "html",
+        value: `
+          <object type="image/svg+xml" data="${svg_path}">
+            <img src="${svg_path}" alt="${value}"/>
+          </object>`,
+      };
+    }
+
+    const svg = NodeCompiler.create().svg({
+      mainFileContent: `
+            #set page(width: auto, height: auto, margin: 10pt)
+            #import "@preview/commute:0.3.0": *
+            #import "@preview/mitex:0.2.5": *
+            #set text( size: ${mathFontSize}pt )
+
+            #mitex(\` ${value} \`)
+          `,
+    });
+
+    fs.writeFileSync(`./public/typst_svg/${key}.svg`, svg);
+
+    return {
+      type: "html",
+      value: `
+          <object type="image/svg+xml" data="${svg_path}">
+            <img src="${svg_path}" alt="${value}"/>
+          </object>`,
+    };
+  } catch (e) {
+    console.log("Error rendering math, error:\n", e, `\nmath:\n${value}`);
+
+    return null;
+  }
 }
